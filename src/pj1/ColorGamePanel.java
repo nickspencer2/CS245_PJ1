52f277d8ec2904df85cdafc648e328a0e52854b3
bcgilmartin <blakegilmartin96@gmail.com>
2017-10-18 19:35:20 -0700

    Color Game complete. Need to fix dimensions. -Blake

--- src/pj1/ColorGamePanel.java
+++ src/pj1/ColorGamePanel.java
@@ -12,6 +12,8 @@ import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -48,16 +50,30 @@ public class ColorGamePanel extends JPanel implements ActionListener {
     private Map<String, JLabel> labels;
     
     private ImageIcon[] colorButtonImages;
+    private ColorJButton[] colorJButton;
     
-    private final boolean DEBUG = true;
+    private final boolean DEBUG = false;
+    
+    private Color randColor;
+    private int score;
+    private int round;
+    private HighScore highScorePanel;
+    private HangmanFrame hangmanFrame;
+    private JPanel cardPanel;
     
     private final String[] colorButtonsNames = {"yellowButton", "greenButton", "purpleButton", "redButton", "blueButton"};
     private final Color[] colors = {Color.YELLOW, Color.GREEN, Color.MAGENTA, Color.RED, Color.BLUE};
     private final String[] colorNames = {"YELLOW", "GREEN", "PURPLE", "RED", "BLUE"};
     private final String[] colorButtonImageNames = {"Images\\yellowbutton.jpg", "Images\\greenbutton.jpg", "Images\\purplebutton.jpg", "Images\\redbutton.jpg", "Images\\bluebutton.jpg"};
     
-    public ColorGamePanel(HangmanFrame hangmanFrame, JPanel cardPanel){
+    public ColorGamePanel(HangmanFrame hFrame, HighScore hsPanel, JPanel cPanel){
         super();
+        score = 0;
+        round = 0;
+        highScorePanel = hsPanel;
+        hangmanFrame = hFrame;
+        cardPanel = cPanel;
+        
         panels = new HashMap<>();
         layouts = new HashMap<>();
         buttons = new HashMap<>();
@@ -69,6 +85,7 @@ public class ColorGamePanel extends JPanel implements ActionListener {
     
     private void initButtonImagesArray(){
         colorButtonImages = new ImageIcon[5];
+        colorJButton = new ColorJButton[5];
         for(int i = 0; i < colorButtonImages.length; i++){
             colorButtonImages[i] = new ImageIcon(colorButtonImageNames[i]);
         }
@@ -146,9 +163,10 @@ public class ColorGamePanel extends JPanel implements ActionListener {
         labels.put("timeLabel", timeLabel);
         //Setup the panel for the label that says the current color
         JPanel colorLabelPane = new JPanel();
+        randColor = colors[r.nextInt(colors.length)];
         JLabel colorLabel = new JLabel(colorNames[r.nextInt(colorNames.length)]);
         colorLabel.setFont(new Font("Arial Black", 0, 25));
-        colorLabel.setForeground(colors[r.nextInt(colors.length)]);
+        colorLabel.setForeground(randColor);
         BoxLayout colorLabelLayout = new BoxLayout(colorLabelPane, BoxLayout.LINE_AXIS);
         colorLabelPane.setLayout(colorLabelLayout);
         colorLabelPane.add(Box.createHorizontalGlue());
@@ -165,6 +183,7 @@ public class ColorGamePanel extends JPanel implements ActionListener {
         JButton redButton = new JButton(colorButtonImages[3]);
         JButton blueButton = new JButton(colorButtonImages[4]);
         JButton[] colorButtons = {yellowButton, greenButton, purpleButton, redButton, blueButton};
+        colorButtonAddAction(colorButtons, colorLabel);
         GridLayout colorButtonsPaneLayout = new GridLayout(5, 5);
         colorButtonsPane.setLayout(colorButtonsPaneLayout);
         addRandomColorButtons(colorButtonsPane, colorButtons);
@@ -228,4 +247,37 @@ public class ColorGamePanel extends JPanel implements ActionListener {
         }
         //TODO listening code here
     }
+    
+    private void colorButtonAddAction(JButton[] colorButtons, JLabel colorLabel) {
+        Random r = new Random();
+        for(int i = 0; i < 5; i++) {
+            Color buttonColor = colors[i];
+            colorButtons[i].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                    if(randColor.equals(buttonColor)) {
+                        score += 100;
+                        System.out.println(score);
+                    }
+                    round++;
+                    if(round < 5) {
+                        randColor = colors[r.nextInt(colors.length)];
+                        colorLabel.setForeground(randColor);
+                        colorLabel.setText(colorNames[r.nextInt(colorNames.length)]);
+                    } else {
+                        highScorePanel.isHighScore(score);
+                        newGame(hangmanFrame, cardPanel, highScorePanel);
+                        hangmanFrame.showPanel("highScorePanel");
+                    }
+                }
+            });
+        }
+    }
+    
+    protected void newGame(HangmanFrame hangmanFrame, JPanel cardPanel, HighScore highScorePanel) {
+        cardPanel.remove(this);
+        ColorGamePanel colorGamePanel = new ColorGamePanel( hangmanFrame, highScorePanel, cardPanel);
+        hangmanFrame.addMenuPanel(colorGamePanel, "colorGamePanel", "colorGameTestButton");
+    }
+    
 }
